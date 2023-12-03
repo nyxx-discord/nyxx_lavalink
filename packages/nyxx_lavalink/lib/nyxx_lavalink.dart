f77778f84613f8b7878e1c0d834484ded0e89bcf
Abitofevrything <mylo.fawcett@gmail.com>
2023-12-03 12:09:26 +0100

    Export package:lavalink

--- packages/nyxx_lavalink/lib/nyxx_lavalink.dart
+++ packages/nyxx_lavalink/lib/nyxx_lavalink.dart
@@ -1,3 +1,5 @@
 export 'src/plugin.dart' show LavalinkPlugin;
 export 'src/lavalink_player.dart' show LavalinkPlayer;
 export 'src/extensions.dart' show LavalinkVoiceChannel;
+
+export 'package:lavalink/lavalink.dart';
