77203902fc8cf930eaf647a44357e10b8570ff01
Abitofevrything <mylo.fawcett@gmail.com>
2023-12-09 14:25:22 +0100

    Document package:lavalink

--- packages/lavalink/lib/src/messages/event.dart
+++ packages/lavalink/lib/src/messages/event.dart
@@ -1,9 +1,14 @@
 import 'package:lavalink/src/messages/message.dart';
 
+/// An event sent by a Lavalink server.
 abstract class LavalinkEvent extends LavalinkMessage {
+  /// The type of this event.
   final String type;
+
+  /// The ID of the guild this event occurred in.
   final String guildId;
 
+  /// Create a new [LavalinkEvent].
   LavalinkEvent({
     required super.client,
     required super.opType,
